da7364250ca7d86e91bee6791363095521971c61
josephizon <josephraymundizon@gmail.com>
2025-06-04 23:53:10 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+// component is any function in JS that returns some kind of JSX code
+// props?/prompts? are parements basically
+function App() {
+
+  return (
+    // fragment is an empty html tag acting as parent 
+    <>
+      
+      <div>hello world</div>
+      
+      
+    </>
+  )
+
+}
+
+export default App
